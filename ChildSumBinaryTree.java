67ffe28a51f3e4d510d108ca5cd55ee2d376ef6a
PraveenSS3B <115014596+PraveenSS3B@users.noreply.github.com>
2023-08-26 18:54:47 +0530

    Basic Tree Traversing

---
+++ ChildSumBinaryTree.java
@@ -0,0 +1,72 @@
+package in.Algo.Tree;
+
+public class ChildSumBinaryTree {
+
+	public static void main(String[] args) {
+		Node root = new Node(20);
+
+		root.left = new Node(8);
+		root.left.left = new Node(3);
+		root.left.right = new Node(5);
+
+		root.right = new Node(12);
+
+		System.out.println(isChildSum(root));
+	}
+
+	private static boolean isChildSum(Node root) {
+
+//		if(root == null) return true;
+//
+//		if (root.key == leftSum + rightSum)
+//			return true;
+//
+//		if (root.left != null) {
+//			isChildSum(root.left, leftSum + root.left.key, rightSum);
+//		}
+//
+//		if (root.right != null) {
+//			isChildSum(root.right, leftSum, rightSum + root.right.key);
+//		}
+//
+//		return false;
+
+		if (root == null)
+			return true;
+
+		if (root.left == null && root.right == null)
+			return true;
+
+		// if root is not a leaf node and root's key is not equal to sum of left and
+		// right child
+		if (root.left != null && root.right != null && root.key != root.left.key + root.right.key)
+			return false;
+
+		// if root is a leaf node and root's key is not equal to sum of left and right
+		// child
+		if (root.left == null && root.right != null && root.key != root.right.key)
+			return false;
+
+		// if root is a leaf node and root's key is not equal to sum of left and right
+		// child
+		if (root.left != null && root.right == null && root.key != root.left.key)
+			return false;
+
+		// if root is not a leaf node and root's key is equal to sum of left and right
+		// child
+		if (root.left != null && root.right != null && root.key == root.left.key + root.right.key)
+			return isChildSum(root.left) && isChildSum(root.right);
+
+		// if root is a leaf node and root's key is equal to sum of left and right child
+		if (root.left == null && root.right != null && root.key == root.right.key)
+			return isChildSum(root.right);
+
+		// if root is a leaf node and root's key is equal to sum of left and right child
+		if (root.left != null && root.right == null && root.key == root.left.key)
+			return isChildSum(root.left);
+
+		return false;
+
+	}
+
+}
---
+++ WidthOfBinaryTree.java
@@ -0,0 +1,54 @@
+package in.Algo.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WidthOfBinaryTree {
+
+	public static void main(String[] args) {
+		Node root = new Node(1);
+
+		root.left = new Node(3);
+
+		root.left.left = new Node(3);
+		root.left.right = new Node(3);
+
+		root.right = new Node(2);
+		root.right.right = new Node(9);
+
+		getMaxWidth(root);
+
+	}
+
+	private static void getMaxWidth(Node root) {
+
+		Queue<Node> q = new LinkedList<Node>();
+
+		q.add(root);
+
+		int max = Integer.MIN_VALUE;
+
+		while (!q.isEmpty()) {
+
+			int size = q.size();
+
+			max = Math.max(max, size);
+
+			for (int i = 0; i < size; i++) {
+				Node curr = q.poll();
+
+				if (curr.left != null)
+					q.add(curr.left);
+
+				if (curr.right != null)
+					q.add(curr.right);
+
+			}
+
+		}
+
+		System.out.println(max);
+
+	}
+
+}
