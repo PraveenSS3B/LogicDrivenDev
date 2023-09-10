04270ebf586b8584a50036da8f46a41bbe845436
PraveenSS3B <praveenkumar66118@gmail.com>
2023-09-10 23:11:01 +0530

    added Question and example for clarity!

--- CheckBalancedBinaryTree.java
+++ CheckBalancedBinaryTree.java
@@ -1,3 +1,24 @@
+/**
+ * Given a binary tree, determine if it is height-balanced:
+ * 
+ * A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node 
+ * never differs by more than one.
+ * Example 1:
+
+				Input: root = [3,9,20,null,null,15,7]
+				Output: true
+				
+Example 2:
+
+				Input: root = [1,2,2,3,3,null,null,4,4]
+				Output: false
+				
+Example 3:
+
+				Input: root = []
+				Output: true
+ */
+
 package in.Algo.Tree.LogicDrivenDev;
 
 public class CheckBalancedBinaryTree {
