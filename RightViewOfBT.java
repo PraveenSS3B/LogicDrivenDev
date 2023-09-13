2f5e2bac6c25684e8f366bf933574cd8ff8df133
PraveenSS3B <praveenkumar66118@gmail.com>
2023-09-13 22:23:04 +0530

    included recursive approach T.C - theta(n); A.S - theta(w)

--- RightViewOfBT.java
+++ RightViewOfBT.java
@@ -7,6 +7,8 @@ import java.util.Queue;
 
 public class RightViewOfBT {
 
+	static int maxLevel = 0;
+
 	public static void main(String[] args) {
 		Node root = new Node(10);
 
@@ -19,11 +21,37 @@ public class RightViewOfBT {
 		root.right.right = new Node(70);
 
 		System.out.println(Arrays.toString(printRightView(root).toArray()));
+
+		System.out.println(Arrays.toString(printRightViewRec(root, 1, new ArrayList<Integer>()).toArray()));
+
 	}
 
+	private static ArrayList<Integer> printRightViewRec(Node root, int lvl, ArrayList<Integer> res) {
+		
+		if(root == null)
+			return res;
+
+		if (lvl > maxLevel) {
+			res.add(root.key);
+			maxLevel = lvl;
+		}
+
+		printRightViewRec(root.right, lvl + 1, res);
+		printRightViewRec(root.left, lvl + 1, res);
+
+		return res;
+
+	}
+
+	/**
+	 * Iterative Approach
+	 * 
+	 * @param root - top node of the tree
+	 * @return the Right nodes of the tree in arrayList
+	 */
 	private static ArrayList<Integer> printRightView(Node root) {
 		ArrayList<Integer> l = new ArrayList<Integer>();
-		
+
 		Queue<Node> q = new LinkedList<>();
 
 		q.add(root);
@@ -35,7 +63,7 @@ public class RightViewOfBT {
 				Node curr = q.poll();
 				if (i == s - 1)
 					l.add(curr.key);
-				
+
 				if (curr.left != null)
 					q.add(curr.left);
 
@@ -43,7 +71,7 @@ public class RightViewOfBT {
 					q.add(curr.right);
 			}
 		}
-		
+
 		return l;
 	}
 
