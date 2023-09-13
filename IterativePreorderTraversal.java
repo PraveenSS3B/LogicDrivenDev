903c5d6c4112d12beaabb97f404c9dc8e4c39b48
PraveenSS3B <praveenkumar66118@gmail.com>
2023-09-13 21:17:36 +0530

    Ignore some white spaces are added.

--- IterativePreorderTraversal.java
+++ IterativePreorderTraversal.java
@@ -8,17 +8,13 @@ public class IterativePreorderTraversal {
 		Node root = new Node(10);
 
 		root.left = new Node(20);
-
-		root.left.right = new Node(50);
-		root.left.right.right = new Node(90);
-
 		root.left.left = new Node(40);
-		root.left.left.left = new Node(70);
-		root.left.left.right = new Node(80);
 
 		root.right = new Node(30);
-
 		root.right.right = new Node(60);
+
+		root.left.right = new Node(50);
+
 //		preorder(root);
 //		System.out.println();
 		preOrder_SpaceOptimized(root);
