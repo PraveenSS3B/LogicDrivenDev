20db4e7a64f65d11c71efecde3b2f5fe5f1149ed
PraveenSS3B <praveenkumar66118@gmail.com>
2023-09-26 17:42:00 +0530

    Fixed Logic of the solution

--- CountNumber_Of_SubTrees_Having_Given_Sum.java
+++ CountNumber_Of_SubTrees_Having_Given_Sum.java
@@ -4,9 +4,9 @@ public class CountNumber_Of_SubTrees_Having_Given_Sum {
 
 	public static void main(String[] args) {
 //		4 0 1 N 2 0 -2 N -4 -1 -3 N 0 -3 -5
-		Node root = VerticalWidth_BT.buildTree("5 -10 3 9 8 -4 7");
+		Node root = VerticalWidth_BT.buildTree("4 0 1 N 2 0 -2 N -4 -1 -3 N 0 -3 -5");
 
-		int X = 7;
+		int X = 5;
 
 		countSubtreesWithSumX(root, X);
 
@@ -25,18 +25,14 @@ public class CountNumber_Of_SubTrees_Having_Given_Sum {
 		if (root == null)
 			return 0;
 
-		if (root.key == X)
-			count++;
-
-		if (root.left == null && root.right == null)
-			return root.key;
-
 		int lv = find(root.left, X);
 		int rv = find(root.right, X);
 
-		if (root.key + lv + rv == X)
+		int sum = lv + rv + root.key;
+
+		if (sum == X)
 			count++;
 
-		return root.key;
+		return sum;
 	}
 }
